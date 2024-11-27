6c9c8a4c515a14b6a707e67935bcd3630958cc78
1cyst7 <5b1g0020@office.stust.edu.tw>
2024-11-27 13:43:28 +0800

    新增 HTML 支援及 BackgroundColorPicker 控制項
    - 將 `applicationToolBar` 的寬度從 200 改為 180
    - 將 `editingToolBar` 的寬度從 500 改為 600
    - 在 `editingToolBar` 中新增 `BackgroundColorPicker` 控制項
    - 設置 `BackgroundColorPicker` 的預設選擇顏色為黑色
    - 修改 `OpenCommand_Executed` 方法，新增 HTML 文件支援
    - 新增 `HtmlToText` 方法，將 HTML 轉換為純文本
    - 修改 `SaveCommand_Executed` 方法，新增 HTML 文件支援
    - 新增 `TextToHtml` 方法，將純文本轉換為 HTML
    - 新增 `BackgroundColorPicker_SelectedColorChanged` 方法

--- 20241120/MyDocumentViewer.xaml.cs
+++ 20241120/MyDocumentViewer.xaml.cs
@@ -22,6 +22,7 @@ namespace _20241120
     public partial class MyDocumentViewer : Window
     {
         Color fontColor = Colors.Black;
+        
         public MyDocumentViewer()
         {
             InitializeComponent();
@@ -34,6 +35,13 @@ namespace _20241120
 
             fontSizeComboBox.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
             fontSizeComboBox.SelectedIndex = 2;
+
+            // 設置 BackgroundColorPicker 的預設選擇顏色為黑色
+            BackgroundColorPicker.SelectedColor = Colors.Black;
+
+
+
+
         }
 
         private void NewCommand_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -45,7 +53,7 @@ namespace _20241120
         private void OpenCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            openFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|HTML Files (*.html;*.htm)|*.html;*.htm|All files (*.*)|*.*";
             openFileDialog.DefaultExt = ".rtf";
             openFileDialog.AddExtension = true;
 
@@ -53,27 +61,61 @@ namespace _20241120
             {
                 FileStream fileStream = new FileStream(openFileDialog.FileName, FileMode.Open);
                 TextRange range = new TextRange(documentRichTextBox.Document.ContentStart, documentRichTextBox.Document.ContentEnd);
-                range.Load(fileStream, DataFormats.Rtf);
+
+                if (openFileDialog.FileName.EndsWith(".html") || openFileDialog.FileName.EndsWith(".htm"))
+                {
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        string htmlContent = reader.ReadToEnd();
+                        range.Text = HtmlToText(htmlContent);
+                    }
+                }
+                else
+                {
+                    range.Load(fileStream, DataFormats.Rtf);
+                }
                 fileStream.Close();
             }
+        }
+        private string HtmlToText(string html)
+        {
+            // 這裡可以使用第三方庫如 HtmlAgilityPack 來解析 HTML
+            // 簡單的實現可以使用正則表達式來去除 HTML 標籤
+            return System.Text.RegularExpressions.Regex.Replace(html, "<.*?>", string.Empty);
         }
 
         private void SaveCommand_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            saveFileDialog.Filter = "Rich Text Format (*.rtf)|*.rtf|HTML Files (*.html;*.htm)|*.html;*.htm|All files (*.*)|*.*";
             saveFileDialog.DefaultExt = ".rtf";
             saveFileDialog.AddExtension = true;
 
             if (saveFileDialog.ShowDialog() == true)
             {
-                TextRange range;
-                FileStream fileStream;
-                range = new TextRange(documentRichTextBox.Document.ContentStart, documentRichTextBox.Document.ContentEnd);
-                fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create);
-                range.Save(fileStream, DataFormats.Rtf);
+                TextRange range = new TextRange(documentRichTextBox.Document.ContentStart, documentRichTextBox.Document.ContentEnd);
+                FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create);
+
+                if (saveFileDialog.FileName.EndsWith(".html") || saveFileDialog.FileName.EndsWith(".htm"))
+                {
+                    string htmlContent = TextToHtml(range.Text);
+                    using (StreamWriter writer = new StreamWriter(fileStream))
+                    {
+                        writer.Write(htmlContent);
+                    }
+                }
+                else
+                {
+                    range.Save(fileStream, DataFormats.Rtf);
+                }
                 fileStream.Close();
             }
+        }
+        private string TextToHtml(string text)
+        {
+            // 這裡可以使用第三方庫如 HtmlAgilityPack 來生成 HTML
+            // 簡單的實現可以使用基本的 HTML 標籤來包裹文本
+            return $"<html><body>{System.Net.WebUtility.HtmlEncode(text).Replace("\n", "<br>")}</body></html>";
         }
         private void documentRichTextBox_SelectionChanged(object sender, RoutedEventArgs e)
         {
@@ -122,6 +164,16 @@ namespace _20241120
         private void clearButton_Click(object sender, RoutedEventArgs e)
         {
             documentRichTextBox.Document.Blocks.Clear();
+        }
+
+        private void BackgroundColorPicker_SelectedColorChanged(object sender, RoutedPropertyChangedEventArgs<Color?> e)
+        {
+            if (e.NewValue.HasValue)
+            {
+                Color selectedColor = e.NewValue.Value;
+                SolidColorBrush brush = new SolidColorBrush(selectedColor);
+                documentRichTextBox.Selection.ApplyPropertyValue(TextElement.BackgroundProperty, brush);
+            }
         }
     }
 }
